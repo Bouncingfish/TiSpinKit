28c0ce9532229ab540a54be2712ac9423c478f1e
Duy Bao Nguyen <bduyng@gmail.com>
2016-08-15 17:04:29 +0700

    update example, add gif

--- example/app.js
+++ example/app.js
@@ -6,34 +6,46 @@
 
 // open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+	backgroundColor: '#3498db'
 });
 
 
 // TODO: write your module tests here
 var TiSpinKit = require('com.bf.TiSpinKit');
-var actInd = TiSpinKit.createActivityIndicator({
-	top: 200, left: 100,
-	size: 20,
-	// STYLES
-	// Plane
-	// CircleFlip
-	// Bounce
-	// Wave
-	// WanderingCubes
-	// Pulse
-	// ChasingDots
-	// ThreeBounce
-	// Circle
-	// 9CubeGrid
-	// WordPress
-	// FadingCircle
-	// FadingCircleAlt
-	// Arc
-	// ArcAlt - default
-	style: "FadingCircle",
-	color: "red"
+var view = Ti.UI.createView({
+	top: 20,
+	height: Ti.UI.SIZE,
+	layout: 'horizontal'
 });
 
-win.add(actInd);
+var styles = [
+	"Plane",
+	"CircleFlip",
+	"Bounce",
+	"Wave",
+	"WanderingCubes",
+	"Pulse",
+	"ChasingDots",
+	"ThreeBounce",
+	"Circle",
+	"9CubeGrid",
+	"WordPress",
+	"FadingCircle",
+	"FadingCircleAlt",
+	"Arc",
+	"ArcAlt" // default
+];
+
+for (var i = 0, maxi = styles.length; i < maxi; i++) {
+	var actInd = TiSpinKit.createActivityIndicator({
+		top: 20, left: 20,
+		width: 70, height: 70, // FIXME: have to use these properties to set frame
+		size: 50,
+		color: "white",
+		style: styles[i]
+	});
+	view.add(actInd);
+}
+
+win.add(view);
 win.open();
