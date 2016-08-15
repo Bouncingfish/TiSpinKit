9235ef9358098dee4fbd67b000823cc889648780
Duy Bao Nguyen <bduyng@gmail.com>
2016-08-15 10:12:45 +0700

    initial commit

---
+++ iphone/Classes/ComBfTiSpinKitModule.h
@@ -0,0 +1,14 @@
+/**
+ * TiSpinKit
+ *
+ * Created by Your Name
+ * Copyright (c) 2016 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface ComBfTiSpinKitModule : TiModule
+{
+}
+
+@end
